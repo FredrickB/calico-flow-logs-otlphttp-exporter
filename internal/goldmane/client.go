0e248d0f5d9d14a30aa56b08295589cf7d94cd88
Fredrick <git@fredrickb.com>
2025-12-15 20:13:10 +0100

    fix: add reconnect after connection drops
    added reconnect after connection drop if Goldmane
    is unailable for some reason.

--- internal/goldmane/client.go
+++ internal/goldmane/client.go
@@ -2,12 +2,19 @@ package goldmane
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	pb "github.com/FredrickB/calico-flow-logs-otlphttp-exporter/v2/gen/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	BACKOFF = 5 * time.Second
 )
 
 type GoldmaneClient struct {
@@ -34,59 +41,78 @@ func NewClient(host, caCertificateFilepath, publicCertFilepath, privateKeyFilepa
 	}, nil
 }
 
-func (client *GoldmaneClient) GetFlow(context context.Context) (*pb.FlowListResult, error) {
-	list, err := client.flowCollectorClient.List(
-		context,
-		&pb.FlowListRequest{},
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to get flows, error: %s. Returning empty list", err)
-	}
-
-	return list, nil
+func (client *GoldmaneClient) Close() error {
+	return client.connection.Close()
 }
 
 func (client *GoldmaneClient) StreamFlows(context context.Context, done chan<- bool) (<-chan *pb.Flow, error) {
-	stream, err := client.flowCollectorClient.Stream(context, &pb.FlowStreamRequest{})
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to create streaming client for flow api %s", err)
-	}
-
 	data := make(chan *pb.Flow)
 
 	go func() {
+		// Begin the outer loop handling initialization
 		for {
-			select {
-			case <-context.Done():
-				log.Println("Context done, closing channel, terminating goroutine")
-				close(data)
-				done <- true
-				return
-			default:
-				var flowResult pb.FlowResult
-				err := stream.RecvMsg(&flowResult)
-				if err == io.EOF {
-					log.Printf("Received EOF, closing channel")
+			stream, err := client.flowCollectorClient.Stream(context, &pb.FlowStreamRequest{})
+
+			if err != nil {
+				log.Printf("Failed to create stream: %s", err)
+				time.Sleep(BACKOFF)
+				continue
+			}
+
+			log.Printf("Stream created")
+
+			for {
+				select {
+				case <-context.Done():
+					log.Println("Context done, closing channel, terminating goroutine")
 					close(data)
 					done <- true
 					return
+				default:
+					var flowResult pb.FlowResult
+					err := stream.RecvMsg(&flowResult)
+
+					if errors.Is(err, io.EOF) {
+						// the sender has nothing more to send.
+						// Terminate execution
+						log.Printf("Received io.EOF, closing channel")
+						close(data)
+						done <- true
+						return
+					}
+
+					if err != nil {
+						if status, ok := status.FromError(err); ok {
+							log.Printf("Failed to receive message. Status: %s. Reconnecting", status.Code())
+							time.Sleep(BACKOFF)
+							// break out of the select to trigger
+							// reinitialization of stream
+							break
+						} else {
+							// this is an error we don't recognize
+							// and presume we can't handle.
+							// Terminate execution
+							log.Printf("Unknown error: %s. Terminating", err)
+							close(data)
+							done <- true
+							return
+						}
+					}
+
+					// pass data from Goldmane and proceed to
+					// next iteration to block and wait for
+					// more data from Goldmane
+					data <- flowResult.Flow
+					continue
 				}
-				if err != nil {
-					log.Printf("Failed to receive message. Error: %s, closing channel", err)
-					close(data)
-					done <- true
-					return
-				}
-				data <- flowResult.Flow
+				// break out of the loop to reinitialize
+				// the stream, only happens if there is
+				// an error received which we believe we
+				// can handle
+				break
 			}
 		}
 	}()
 
 	return data, nil
-}
-
-func (client *GoldmaneClient) Close() error {
-	return client.connection.Close()
 }
