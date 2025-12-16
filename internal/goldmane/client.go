4f9ffe17390bd9e94269043f64ec1958c9c98fd5
Fredrick Biering <8116958+FredrickB@users.noreply.github.com>
2025-12-16 19:20:25 +0100

    feat: implement reconnect due to loss of connection to Goldmane (#93)
    - added reconnect after connection drop if Goldmane
    is unavailable for some reason
    - added a new environment variable `RECONNECT_WAIT_TIME_IN_SECONDS`
    which can be used to set how many seconds to wait before
    attempting to reconnect after recoverable connection failure

--- internal/goldmane/client.go
+++ internal/goldmane/client.go
@@ -2,12 +2,15 @@ package goldmane
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	pb "github.com/FredrickB/calico-flow-logs-otlphttp-exporter/v2/gen/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 type GoldmaneClient struct {
@@ -34,52 +37,33 @@ func NewClient(host, caCertificateFilepath, publicCertFilepath, privateKeyFilepa
 	}, nil
 }
 
-func (client *GoldmaneClient) GetFlow(context context.Context) (*pb.FlowListResult, error) {
-	list, err := client.flowCollectorClient.List(
-		context,
-		&pb.FlowListRequest{},
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to get flows, error: %s. Returning empty list", err)
-	}
-
-	return list, nil
+func (client *GoldmaneClient) Close() error {
+	return client.connection.Close()
 }
 
-func (client *GoldmaneClient) StreamFlows(context context.Context, done chan<- bool) (<-chan *pb.Flow, error) {
-	stream, err := client.flowCollectorClient.Stream(context, &pb.FlowStreamRequest{})
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to create streaming client for flow api %s", err)
-	}
-
+func (client *GoldmaneClient) StreamFlows(context context.Context, done chan<- bool, reconnectWaitTime time.Duration) (<-chan *pb.Flow, error) {
 	data := make(chan *pb.Flow)
 
 	go func() {
 		for {
-			select {
-			case <-context.Done():
-				log.Println("Context done, closing channel, terminating goroutine")
+			stream, err := client.flowCollectorClient.Stream(context, &pb.FlowStreamRequest{})
+
+			if err != nil {
+				log.Printf("Failed to create stream: %s. Sleeping for %s", err, reconnectWaitTime)
+				time.Sleep(reconnectWaitTime)
+				continue
+			}
+
+			log.Printf("Stream created")
+
+			// block until streaming fails, then check return
+			// value to see if reconnect should be done or if
+			// the execution should be terminated
+			reconnect, _ := streamFlowsUntilError(context, stream, data)
+			if !reconnect {
 				close(data)
 				done <- true
 				return
-			default:
-				var flowResult pb.FlowResult
-				err := stream.RecvMsg(&flowResult)
-				if err == io.EOF {
-					log.Printf("Received EOF, closing channel")
-					close(data)
-					done <- true
-					return
-				}
-				if err != nil {
-					log.Printf("Failed to receive message. Error: %s, closing channel", err)
-					close(data)
-					done <- true
-					return
-				}
-				data <- flowResult.Flow
 			}
 		}
 	}()
@@ -87,6 +71,45 @@ func (client *GoldmaneClient) StreamFlows(context context.Context, done chan<- b
 	return data, nil
 }
 
-func (client *GoldmaneClient) Close() error {
-	return client.connection.Close()
+// start streaming flow logs to channel `data` until error is
+// received from grpc. If there is a possibillity for recovery
+// by reconnecting, flag it to the caller using `reconnect`
+func streamFlowsUntilError(context context.Context, stream grpc.ServerStreamingClient[pb.FlowResult], data chan<- *pb.Flow) (reconnect bool, err error) {
+	// start out with the assumption that
+	// reconnecting won't be done for the
+	// majority of errors
+	reconnect = false
+
+	log.Printf("Streaming logs...")
+
+	for {
+		select {
+		case <-context.Done():
+			log.Println("Context done, terminating")
+			return
+		default:
+			var flowResult pb.FlowResult
+			err = stream.RecvMsg(&flowResult)
+
+			if errors.Is(err, io.EOF) {
+				// the sender has nothing more to send, terminate
+				log.Printf("Received EOF, terminating")
+				return
+			}
+
+			if err != nil {
+				if status, ok := status.FromError(err); ok {
+					// known error, reconnect
+					log.Printf("Status received: %s, trigger reconnect", status.Code())
+					reconnect = true
+				} else {
+					// unrecognizeable error, terminate
+					log.Printf("Unknown error: %s, terminating", err)
+				}
+				return
+			}
+
+			data <- flowResult.Flow
+		}
+	}
 }
--- internal/util/util.go
+++ internal/util/util.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/FredrickB/calico-flow-logs-otlphttp-exporter/v2/internal/goldmane"
 	"github.com/FredrickB/calico-flow-logs-otlphttp-exporter/v2/internal/otlp"
 )
 
-func StartLogStreaming(context context.Context, client *goldmane.GoldmaneClient, logger *otlp.Logger) (chan bool, error) {
+func StartLogStreaming(context context.Context, client *goldmane.GoldmaneClient, logger *otlp.Logger, reconnectWaitTime time.Duration) (chan bool, error) {
 	done := make(chan bool)
 
-	data, err := client.StreamFlows(context, done)
+	data, err := client.StreamFlows(context, done, reconnectWaitTime)
 	if err != nil {
 		return nil, fmt.Errorf("error during start of log streaming: %s", err)
 	}
@@ -32,4 +34,13 @@ func Cleanup(context context.Context, client *goldmane.GoldmaneClient, logger *o
 
 func LogEnvironmentVariable(variable, value string) {
 	log.Printf("%s set to %s", variable, value)
+}
+
+func ParseSecondsStringValue(secondsAsString string, defaultValue time.Duration) time.Duration {
+	secondsParsed, err := strconv.Atoi(secondsAsString)
+	if err != nil {
+		return defaultValue
+	} else {
+		return time.Duration(secondsParsed) * time.Second
+	}
 }
